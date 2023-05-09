7b0ee481b47c5d03d4e84b6650e467fb065f0996
Ofori Gyimah <rabbioforigyimah@gmail.com>
2023-05-09 23:11:34 +0000

    task 101 fixed

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     new = []
-    for l in str:
+    for c in str:
         if len(str) < n or n < 0:
             return str
         if l != str[n]:
-            new.append(l)
+            new.append(c)
     return "".join(new)
